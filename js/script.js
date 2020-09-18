36459373970929bdbae3215ec2ebc7822bdc12ee
nuzulzen <nuzulzen@gmail.com>
2020-09-18 20:43:35 +0700

    fix js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "20px";
-    //document.getElementById("navbar").style.background = "rgb(25,25,25)";
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("navbar").style.top = "20px";
+//     //document.getElementById("navbar").style.background = "rgb(25,25,25)";
 
-    //document.getElementById("logo").style.fontSize = "25px";
-  } else {
-    document.getElementById("navbar").style.top = "100px";
-   // document.getElementById("navbar").style.background = "transparant";
-    //document.getElementById("logo").style.fontSize = "35px";
-  }
-}
+//     //document.getElementById("logo").style.fontSize = "25px";
+//   } else {
+//     document.getElementById("navbar").style.top = "100px";
+//    // document.getElementById("navbar").style.background = "transparant";
+//     //document.getElementById("logo").style.fontSize = "35px";
+//   }
+// }
 
 /*$(document).ready(function(){
     $(window).scroll(function() { 
